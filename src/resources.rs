5a9675224393c837255bfd44fbb2e01152c7e1f1
Washington Irving <iamwashingyou@gmail.com>
2024-09-12 22:58:27 +0300

    add mutable access to resources

--- src/resources.rs
+++ src/resources.rs
@@ -20,6 +20,14 @@ impl Resources {
 
         None
     }
+
+    pub fn get_mut<T: Any>(&mut self) -> Option<&mut T> {
+        if let Some(data) = self.data.get_mut(&TypeId::of::<T>()) {
+            return data.downcast_mut();
+        }
+
+        None
+    }
 }
 
 #[cfg(test)]
@@ -51,5 +59,19 @@ mod tests {
         let extracted_world_width = resources.get_ref::<WorldWidth>().unwrap();
 
         assert_eq!(extracted_world_width.0, 420.);
+    }
+
+    #[test]
+    fn get_resource_mutably() {
+        let mut resources = Resources::default();
+        let world_width = WorldWidth(420.);
+
+        resources.add(world_width);
+
+        let world_width = resources.get_mut::<WorldWidth>().unwrap();
+
+        world_width.0 -= 351.0;
+
+        assert_eq!(resources.get_ref::<WorldWidth>().unwrap().0, 69.);
     }
 }
--- tests/resources_test.rs
+++ tests/resources_test.rs
@@ -1,5 +1,7 @@
 use secs::World;
 
+struct FPSResource(pub u32);
+
 #[test]
 fn create_ang_get_resource_immutably() {
     let mut world = World::new();
@@ -11,4 +13,15 @@ fn create_ang_get_resource_immutably() {
     assert_eq!(fps.0, 60);
 }
 
-struct FPSResource(pub u32);
+#[test]
+fn get_resources_mutably() {
+    let mut world = World::new();
+
+    world.add_resource(FPSResource(60));
+
+    let fps: &mut FPSResource = world.get_resource_mut::<FPSResource>().unwrap();
+
+    fps.0 += 60;
+
+    assert_eq!(world.get_resource::<FPSResource>().unwrap().0, 120);
+}
